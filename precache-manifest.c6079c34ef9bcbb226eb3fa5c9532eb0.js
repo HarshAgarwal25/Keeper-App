d6ec62f8b46a4d7fd2ff4b684a48b4cbf7ee4631
Harsh Agarwal <252001HARSH@GMAIL.COM>
2021-08-19 18:07:45 +0530

    Updates

---
+++ precache-manifest.c6079c34ef9bcbb226eb3fa5c9532eb0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74c6c7c3e4a029a08082ebc52a2a01b0",
+    "url": "/ReactJs/index.html"
+  },
+  {
+    "revision": "daf87c252a5386ee45f9",
+    "url": "/ReactJs/static/js/2.64d65bb3.chunk.js"
+  },
+  {
+    "revision": "4a9366f0f05f9807e14d",
+    "url": "/ReactJs/static/js/main.88eeae52.chunk.js"
+  },
+  {
+    "revision": "82e2d3a84a2724540860",
+    "url": "/ReactJs/static/js/runtime-main.a5744fe3.js"
+  }
+]);
